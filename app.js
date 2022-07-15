b20b6415df10753433fc26531f19fa982d9fd885
Cap <90457528+NeoShubh@users.noreply.github.com>
2022-07-16 00:40:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  // $('.clicks').on('click', function(){
+  //     $('#box').addClass('show');
+  // })
+
+  $(".clicks").on("click", function () {
+    $("#box").toggleClass("show");
+  });
+
+  $(".close").on("click", function () {
+    $("#box").removeClass("show");
+  });
+
+  $(".submission").on("click", function () {
+    $("#box").removeClass("show");
+  });
+});
